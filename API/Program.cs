7bf70fac7607e0f61e4af2087e255cc09a77c9d2
Xiaobin Zhu <sam.zhu992@gmail.com>
2023-02-02 22:24:00 -0700

    end of section 3

--- API/Program.cs
+++ API/Program.cs
@@ -1,19 +1,32 @@
 using API.Data;
 using Microsoft.EntityFrameworkCore;
 
+var MyAllowSpecificOrigins = "_myAllowSpecificOrigins";
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
 builder.Services.AddDbContext<DataContext>(opt => 
 {
     opt.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
 
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy(name: MyAllowSpecificOrigins,
+                        policy =>
+                        {
+                            policy.WithOrigins("https://localhost:4200");
+                        });
+});
+
+builder.Services.AddControllers();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+//app.UseCors(builder => builder.AllowAnyHeader().AllowAnyMethod().WithOrigins("https://localhost:4200"));
+app.UseCors(MyAllowSpecificOrigins);
+
 app.MapControllers();
 
 app.Run();
